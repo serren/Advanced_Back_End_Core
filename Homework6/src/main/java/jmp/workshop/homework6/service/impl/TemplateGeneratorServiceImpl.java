376a6bf5b827a8efaafeb249f5e668dec05ddafe
Sergei_Meranovich <sergei_meranovich@epam.com>
2022-08-05 12:36:13 +0300

    HW6: Passed test testOnePlaceholderReplace.

--- Homework6/src/main/java/jmp/workshop/homework6/service/impl/TemplateGeneratorServiceImpl.java
+++ Homework6/src/main/java/jmp/workshop/homework6/service/impl/TemplateGeneratorServiceImpl.java
@@ -11,8 +11,56 @@ import java.util.stream.Collectors;
 @Service
 public class TemplateGeneratorServiceImpl implements TemplateGeneratorService {
 
+    public static final String START_MARKER = "#{";
+    public static final String END_MARKER = "}";
+
     @Override
     public String process(String template, String value) throws IllegalArgumentException {
-        return null;
+        if (!containsPlaceholders(template)) {
+            return template;
+        }
+        Map<String, String> placeHolders = getValues(value);
+        StringBuilder result = new StringBuilder();
+        int startIndex = template.indexOf(START_MARKER);
+        int endIndex = template.indexOf(END_MARKER, startIndex);
+        int currentPosition = 0;
+        while (startIndex > 0 && endIndex > 0) {
+            result.append(template.substring(currentPosition, startIndex));
+            String placeHolder = template.substring(startIndex, endIndex + 1);
+            String placeHolderValue = placeHolders.get(placeHolder);
+            result.append(placeHolderValue);
+            currentPosition = endIndex + 1;
+            startIndex = template.indexOf(START_MARKER, startIndex + endIndex + 1);
+            endIndex = template.indexOf(END_MARKER, startIndex);
+        }
+        if (currentPosition < template.length()) {
+            result.append(template.substring(currentPosition, template.length()));
+        }
+        return result.toString();
+    }
+
+    private Map<String, String> getValues(String value) {
+        return Arrays.stream(StringUtils.defaultString(value)
+                .split("\n"))
+                .map(p -> p.split("="))
+                .map(s -> {
+                    String[] values = new String[2];
+                    values[0] = s[0];
+                    values[1] = s.length > 1 ? s[1] : "";
+                    return values;
+                })
+                .collect(Collectors.toMap(s -> s[0], s -> s[1]));
+    }
+
+    private boolean containsPlaceholders(String template) {
+        if (StringUtils.isBlank(template)) {
+            return false;
+        }
+        int startIndex = template.indexOf(START_MARKER);
+        if (startIndex < 0) {
+            return false;
+        }
+        int endIndex = template.indexOf(END_MARKER, startIndex);
+        return endIndex > 0;
     }
 }
